443f76ec851299af07fb818951382c3a09cd3c51
adriano barbosa <adriano.barbosa100@gmail.com>
2023-03-14 21:39:48 -0300

    update text

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
-import img1 from './assets/img/1.png'
-import img2 from './assets/img/2.png'
-import aboutpic from './components/Access/adriano.jpg'
+import img1 from './assets/img/1.png';
+import img2 from './assets/img/2.png';
+import aboutpic from './components/Access/adriano.jpg';
 
 const header = {
   // all the properties are optional - can be left empty or deleted
@@ -8,13 +8,18 @@ const header = {
   title: 'JS.',
 }
 
+const dataDeReferencia = new Date(2017, 2, 15); // 15 de março de 2016
+const dataAtual = new Date();
+const diferencaEmAnos = dataAtual.getFullYear() - dataDeReferencia.getFullYear();
+const experiencia = diferencaEmAnos === 1 ? "um ano" : `${diferencaEmAnos} anos`;
+
 const about = {
   photo: aboutpic,
   // all the properties are optional - can be left empty or deleted
   name: 'Adriano Barbosa',
   role: 'Full stack developer',
   description:
-    `Sou graduado em Análise e Desenvolvimento de Sistemas e tenho mais de seis anos de experiência como Programador e Desenvolvedor, atuando diretamente no mercado. Minha paixão pela programação começou em 2016 e desde então venho aprimorando minhas habilidades e conhecimentos através de projetos externos, incluindo desenvolvimento de websites, aplicativos e sistemas.
+    `Sou graduado em Análise e Desenvolvimento de Sistemas e tenho mais de ${experiencia} de experiência como Programador e Desenvolvedor, atuando diretamente no mercado. Minha paixão pela programação começou em 2016 e desde então venho aprimorando minhas habilidades e conhecimentos através de projetos externos, incluindo desenvolvimento de websites, aplicativos e sistemas.
 
     Iniciei minha carreira na Secretaria da Educação em Anápolis, onde tive a oportunidade de consolidar minhas competências e me desafiar profissionalmente. Atualmente, sou um desenvolvedor Full stack, habilidoso em trabalhar com Nestjs, Express, mongoDB, SQL, PostgreSQL no lado servidor (Back-end) e Nextjs, React, diversos frameworks css no lado cliente (Front-end). Sempre busco me manter atualizado e continuamente aprendendo para aprimorar minha técnica.
     
@@ -37,7 +42,7 @@ const projects = [
     description:
       'Projeto realizado para a prefeitura de Anápolis, utilizei o Nextjs e o framwork Chakra UI',
     stack: ['Correios API', 'React.js', 'Redux Tookit', 'Chackra UI', 'Docker'],
-    sourceCode: 'https://github.com/adrianobarbosa1/frontend-mlmh',
+    sourceCode: '.',
     livePreview: 'http://meuloteminhahistoria.anapolis.go.gov.br/',
   },
   {
@@ -47,8 +52,8 @@ const projects = [
     description:
       'RESTful API Meu lote minha historia, projeto realizado para a prefeitura de Anápolis, backend com mongoDB, Atuhetication de usuario, upload de imagens usando o Multer, validação de dados com Joi, documentado com Swagger e swagger-ui-express, além de outras bibliotecas ',
     stack: ['Express.js', 'MongoDB', 'Nodejs', 'Docker'],
-    sourceCode: 'https://github.com/adrianobarbosa1/backend-mlmh',
-    livePreview: 'https://github.com/adrianobarbosa1/backend-mlmh',
+    sourceCode: '.',
+    livePreview: '.',
   },
   // {
   //   name: 'Resource Planner',
@@ -88,5 +93,5 @@ const contact = {
   email: 'adriano.barbosa100@gmail.com',
 }
 
-export { header, about, projects, skills, contact }
+export { header, about, projects, skills, contact };
 
