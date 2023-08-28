459fdcb09822f67822944711dabdade677f1850a
Adriano Barbosa <adriano.barbosa100@email.com>
2023-08-28 14:15:17 -0300

    feat: add links

--- portfolio-next/src/app/page.tsx
+++ portfolio-next/src/app/page.tsx
@@ -1,14 +1,20 @@
 import About from "@/components/About";
+import Contato from "@/components/Contato";
+import Habilidades from "@/components/Habilidades";
 import Hero from "@/components/Hero";
+import Portfolio from "@/components/Portfolio";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center max-w-6xl w-11/12 mx-auto mt-4">
+    <main
+      id="home"
+      className="flex flex-col items-center max-w-6xl w-11/12 mx-auto mt-4 px-4"
+    >
       <Hero />
       <About />
-      <h1>habilidades</h1>
-      <h1>Portfolio</h1>
-      <h1>Contato</h1>
+      <Habilidades />
+      <Portfolio />
+      <Contato />
     </main>
   );
 }
