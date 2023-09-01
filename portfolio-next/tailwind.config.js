0304f01f293ffa2bbbcfadf9ca32df572867a404
Adriano Barbosa <adriano.barbosa100@email.com>
2023-09-01 15:12:57 -0300

    feat: add shadcn variables

--- portfolio-next/tailwind.config.js
+++ portfolio-next/tailwind.config.js
@@ -18,17 +18,47 @@ module.exports = {
     extend: {
       colors: {
         // --------------LIGHT---------------------
-        lightBg: "#FCFCFC",
         lightBgAlt: '#FFFFFF',
         lightTexto: "#555555",
         lightTextoTitulo: '#444444',
-        colorPrimary: "#2978B5",
-        // --------------DARK---------------------
-        darkBg: "#23283E",
+        // --------------DARK----------------------
         darkBgAlt: '#2A2F4C',
         darkTexto: "#BDBDDD",
         darkTextoTitulo: '#CDCDFF',
-        darkcolorPrimary: '#90A0D9',
+        // --------------SHADCN--------------------
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
