f4b7215100451660a0ff37dcb4b685373ff3bf67
Adriano Barbosa <adriano.barbosa100@email.com>
2023-09-04 20:37:49 -0300

    feat: add portfolio

--- portfolio-next/src/components/Portfolio/index.tsx
+++ portfolio-next/src/components/Portfolio/index.tsx
@@ -1,9 +1,11 @@
 "use client";
 import Aos from "aos";
+import "aos/dist/aos.css";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect } from "react";
 import { FaGithub } from "react-icons/fa";
+import { dataPortfolio } from "./dataPortfolio";
 
 const Portfolio = () => {
   useEffect(() => {
@@ -12,48 +14,60 @@ const Portfolio = () => {
 
   return (
     <div id="portfolio" className="items-center flex flex-col mt-10">
-      <h2 className="mt-10 text-3xl font-bold text-lightTextoTitulo">
+      <h2 className="mt-10 text-3xl font-bold text-lightTextoTitulo dark:text-darkTextoTitulo">
         Portfolio
       </h2>
+      {dataPortfolio.map((item, i) => (
+        <div
+          className={` flex flex-col sm:flex-row sm:w-full sm:gap-12 sm:mb-24 ${
+            i % 2 === 0 ? "" : "sm:flex-row-reverse bg-gray-100" // Aplica flex-row-reverse para indices pares
+          }`}
+        >
+          <div
+            className=" mt-10 h-72 w-full sm:h-96 sm:w-96 sm:m-2 relative justify-content align-center"
+            data-aos={`${i % 2 === 0 ? "fade-right" : "fade-left"}`} // Aplica fade-left para indices pares
+          >
+            <Image
+              className="object-cover"
+              fill={true}
+              src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
+              alt=""
+            />
+          </div>
 
-      <div className=" w-full gap-12 flex mt-12 mb-24">
-        <div className=" flex-1 flex flex-col ">
-          <h1 className="text-4xl sm:text-5xl text-defaultPrimary font-bold text-center">
-            Creative portfolio
-          </h1>
-          <p className="my-6 text-xl text-center">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
-            vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate
-            ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet
-            fugiat tenetur?
-          </p>
-          <div className="flex justify-center gap-x-3"></div>
+          <div className=" mt-10  flex-1 flex flex-col gap-5 justify-center">
+            <h1 className="text-primary dark:text-primary leading-6 font-bold  text-3xl sm:text-5xl text-center">
+              {item.title}
+            </h1>
+            <p className="sm:my-6 m-6 text-xl text-center dark:text-darkTexto">
+              {item.desc}
+            </p>
 
-          <ul className="about__contact center flex space-x-6 justify-center">
-            <li className=" my-1 pb-2 relative link_animation">
-              <button className="hover:bg-primary">veja mais</button>
-            </li>
-            <li className=" my-1 pb-2 relative link_animation">
-              <Link
-                href="https://github.com/adrianobarbosa1"
-                aria-label="github"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaGithub className="w-8 h-8  hover:text-defaultPrimary hover:transition-all hover:duration-300" />
-              </Link>
-            </li>
-          </ul>
+            <ul className="about__contact center flex space-x-6 justify-center">
+              <li>
+                <Link href={item.href}>
+                  <button className="group relative border-2 border-primary h-10 w-48 overflow-hidden text-lg ">
+                    <div className="absolute inset-0 w-3 bg-primary transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+                    <span className="relative text-black group-hover:text-white dark:text-darkTexto">
+                      Veja mais
+                    </span>
+                  </button>
+                </Link>
+              </li>
+              <li className="my-1 pb-2 relative link_animation">
+                <Link
+                  href="https://github.com/adrianobarbosa1"
+                  aria-label="github"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithub className="w-8 h-8  hover:text-primary hover:transition-all hover:duration-300" />
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="flex-1 h-96 w-96 relative">
-          <Image
-            className="object-cover"
-            fill={true}
-            src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
-            alt=""
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
