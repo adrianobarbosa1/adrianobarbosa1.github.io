02b64af8a96600add7ce3bb4d69f65e7b834aeb8
Adriano Barbosa <adriano.barbosa100@email.com>
2023-09-06 10:07:37 -0300

    feat: POR-6 add tela contato

--- portfolio-next/src/components/Contato/index.tsx
+++ portfolio-next/src/components/Contato/index.tsx
@@ -1,37 +1,74 @@
-import Link from "next/link";
-
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+"use client";
+import { useEffect } from "react";
+import InputMask from "react-input-mask";
 
 const Contato = () => {
+  const scrollToInput = () => {
+    const firstInput = document.querySelector("input");
+    const offset = 100; // Ajuste a quantidade de rolagem conforme necessário
+    if (firstInput) {
+      window.scrollTo(0, firstInput.offsetTop - offset);
+    }
+  };
+
+  useEffect(() => {
+    const firstInput = document.querySelector("input"); // Seleciona o primeiro input
+    if (firstInput) {
+      firstInput.addEventListener("focus", scrollToInput);
+    }
+
+    // Remova o ouvinte de foco quando o componente é desmontado
+    return () => {
+      if (firstInput) {
+        firstInput.removeEventListener("focus", scrollToInput);
+      }
+    };
+  }, []);
+
   return (
-    <div id="contact" className="items-center flex flex-col mt-10">
-      <h2 className="mt-10 text-3xl font-bold text-lightTextoTitulo">
+    <div id="contact" className="items-center flex flex-col mt-10  w-full">
+      <h2 className="mt-10 text-3xl font-bold text-lightTextoTitulo dark:text-darkTextoTitulo">
         Contato
       </h2>
 
-      <div className="about__contact center">
-        <>
-          <Link
-            href="https://github.com/adrianobarbosa1"
-            aria-label="github"
-            className="link link--icon"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaGithub />
-          </Link>
+      <form className=" ">
+        <div className="flex flex-col mt-10 sm:flex-row w-full">
+          <div className="sm:w-1/2 sm:mx-5">
+            <input
+              type="text"
+              placeholder="nome"
+              className="custom-input w-full "
+            />
+            <input
+              type="email"
+              placeholder="email"
+              className="custom-input mt-3 w-full  "
+            />
 
-          <Link
-            href="https://www.linkedin.com/in/adrianobarbosa-dev/"
-            aria-label="linkedin"
-            className="link link--icon"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaLinkedin />
-          </Link>
-        </>
-      </div>
+            <InputMask
+              mask="(99)9-9999-9999"
+              type="tel"
+              id="whatsapp"
+              placeholder="whatsapp"
+              className="custom-input mt-3 w-full"
+            />
+          </div>
+          <div className=" sm:w-1/2 sm:mx-5 ">
+            <textarea
+              placeholder="mensagem..."
+              className="custom-input mt-3 sm:mt-0 w-full sm:h-full"
+            />
+          </div>
+        </div>
+        <div className="flex center justify-center mt-10">
+          <button className="group relative border-2 border-primary h-10 w-48 overflow-hidden text-lg ">
+            <div className="absolute inset-0 w-3 bg-primary transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+            <span className="relative text-black group-hover:text-white dark:text-darkTexto">
+              Enviar
+            </span>
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
