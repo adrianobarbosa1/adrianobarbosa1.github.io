40911be6bfc21aa2d759e5eadfe48a83fa7604d0
Adriano Barbosa <adriano.barbosa100@email.com>
2023-09-06 16:57:52 -0300

    feat: POR-13 add footer in layout

--- portfolio-next/src/app/page.tsx
+++ portfolio-next/src/app/page.tsx
@@ -1,20 +1,19 @@
 import About from "@/components/About";
 import Contato from "@/components/Contato";
+import Footer from "@/components/Footer";
 import Habilidades from "@/components/Habilidades";
 import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
 
 export default function Home() {
   return (
-    <main
-      id="home"
-      className="flex flex-col items-center max-w-6xl w-11/12 mx-auto mt-4 px-4"
-    >
+    <main id="home" className="flex flex-col items-center  mx-auto mt-4">
       <Hero />
       <About />
       <Habilidades />
       <Portfolio />
       <Contato />
+      <Footer />
     </main>
   );
 }
