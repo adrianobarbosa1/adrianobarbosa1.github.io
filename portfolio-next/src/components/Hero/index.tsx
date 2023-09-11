a1d5999277dae4d58357c54602f3e3b466761e6a
Adriano Barbosa <adriano.barbosa100@email.com>
2023-09-11 19:31:16 -0300

    feat: change Image props next

--- portfolio-next/src/components/Hero/index.tsx
+++ portfolio-next/src/components/Hero/index.tsx
@@ -2,22 +2,22 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="items-center flex flex-col">
+    <section className="items-center flex flex-col mt-20">
       <div className="h-64 w-64 relative mt-10 border-2 border-primary dark:border-primary rounded-full">
         <Image
           src="/adriano.png"
-          layout="fill"
-          quality={100}
           alt="Adriano Barbosa"
-          objectFit="cover"
-          className="rounded-full"
+          className="rounded-full object-cover"
+          quality={100}
+          fill={true}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         />
       </div>
 
       <h1 className=" text-primary dark:text-primary leading-6 font-bold mt-10 text-4xl sm:text-6xl">
         Adriano Barbosa.
       </h1>
-    </div>
+    </section>
   );
 };
 
