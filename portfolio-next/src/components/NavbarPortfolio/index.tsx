94443ab3d7548643c270c479fe06923a3a2cc264
Adriano Barbosa <adriano.barbosa100@email.com>
2023-09-11 19:32:06 -0300

    feat: add mobile navbar

--- portfolio-next/src/components/NavbarPortfolio/index.tsx
+++ portfolio-next/src/components/NavbarPortfolio/index.tsx
@@ -1,85 +1,97 @@
 "use client";
-import { useStateContext } from "@/contexts/ContextProvider";
 import { useTheme } from "next-themes";
 import Link from "next/link";
-import { useEffect } from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { BiSolidSun } from "react-icons/bi";
 import { FaMoon } from "react-icons/fa";
-import { links } from "./data.links";
+import MenuOverlay from "./MenuOverlay";
+import NavLink from "./NavLink";
+import dataLinks from "./data.links";
 
 const NavbarPortfolio = () => {
-  const {
-    activeMenu,
-    setActiveMenu,
-    handleClick,
-    isClicked,
-    setScreenSize,
-    screenSize,
-    currentColor,
-  } = useStateContext();
   const { theme, setTheme } = useTheme();
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
 
   useEffect(() => {
-    const handleResize = () => setScreenSize(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
+    // Função para lidar com a rolagem da página
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    // Adicionar um ouvinte de evento para a rolagem
+    window.addEventListener("scroll", handleScroll);
+
+    // Remover o ouvinte de evento quando o componente for desmontado
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
-  useEffect(() => {
-    if (screenSize !== undefined && screenSize <= 900) {
-      setActiveMenu(false);
-    } else {
-      setActiveMenu(true);
-    }
-  }, [screenSize]);
-
-  const handleActiveMenu = () => setActiveMenu(!activeMenu);
-
   return (
-    <nav className=" flex justify-between my-8 md:my-12 mx-auto max-w-6xl w-11/12">
-      <ul className="flex">
-        <li className="ml-6 my-1">
-          <Link
-            href="/"
-            className="text-2xl font-bold pb-2 relative link_animation text-primary dark:text-darkTexto"
-          >
-            Blog
-          </Link>
-        </li>
-        <li className="ml-6 my-1 mt-2">
-          <button title="Menu" onClick={handleActiveMenu} color={currentColor}>
-            <AiOutlineMenu />
-          </button>
-        </li>
-      </ul>
+    <nav
+      className={`fixed mx-auto top-0 left-0 right-0 z-10  ${
+        scrolling
+          ? "bg-white dark:bg-slate-900 drop-shadow-md"
+          : "bg-transparent"
+      } transition-all duration-500`}
+    >
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Link
+          href="/"
+          className="text-2xl font-bold pb-2 text-primary dark:text-darkTexto
+          relative w-fit after:block after:content-[''] after:absolute after:h-[3px] after:bg-primary after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left
+          "
+        >
+          Blog
+        </Link>
 
-      <ul className=" flex mr-2">
-        {links.map((link) => (
-          <li key={link.id} className="ml-6 my-1">
-            <Link
-              href={link.url}
-              key={link.id}
-              className="pb-2 relative text-lightTexto lowercase font-medium link_animation hover:text-primary dark:text-darkTexto"
+        <div className="mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2 border rounded border-primary text-slate-200 hover:text-white hover:border-white"
             >
-              {link.title}
-            </Link>
-          </li>
-        ))}
-        <li className="ml-6 my-1 mt-2 hover:text-colorPrimary">
-          <button
-            type="button"
-            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-          >
-            {theme === "light" ? (
-              <FaMoon className="text-lightTextoTitulo hover:text-primary" />
-            ) : (
-              <BiSolidSun className="dark:text-darkTexto hover:dark:text-primary" />
-            )}
-          </button>
-        </li>
-      </ul>
+              <AiOutlineMenu className="h-5 w-5 text-primary" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-primary text-slate-200 hover:text-white hover:border-white"
+            >
+              <AiOutlineClose className="h-5 w-5 text-primary" />
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+            {dataLinks.map((link) => (
+              <li key={link.id}>
+                <NavLink href={link.href} title={link.title} />
+              </li>
+            ))}
+
+            <li>
+              <button
+                type="button"
+                onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+              >
+                {theme === "light" ? (
+                  <FaMoon className="text-lightTextoTitulo hover:text-primary" />
+                ) : (
+                  <BiSolidSun className="dark:text-darkTexto hover:dark:text-primary" />
+                )}
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {navbarOpen ? <MenuOverlay dataLinks={dataLinks} /> : null}
     </nav>
   );
 };
