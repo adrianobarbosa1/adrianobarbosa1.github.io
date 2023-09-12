1e667d03339472c25c6e2e86b697603cb19cc868
Adriano Barbosa <adriano.barbosa100@email.com>
2023-09-11 23:34:40 -0300

    feat: POR-18 add data skills

---
+++ portfolio-next/src/components/Habilidades/dataSkills.tsx
@@ -0,0 +1,30 @@
+import { AiOutlineMobile } from "react-icons/ai";
+import { BiInfinite, BiLogoReact } from "react-icons/bi";
+import { FaNodeJs } from "react-icons/fa";
+
+export const dataSkills = [
+  {
+    id: 1,
+    title: "Frontend",
+    desc: `No frontend, domino React, Next.js e Tailwind CSS para criar interfaces dinâmicas e responsivas. Também sou experiente em JavaScript, TypeScript, Sass, Bootstrap e Material UI, permitindo-me desenvolver aplicações atraentes e funcionais.`,
+    icon: BiLogoReact,
+  },
+  {
+    id: 2,
+    title: "Mobile",
+    desc: "No desenvolvimento mobile, sou proficiente em React Native e Expo, permitindo a criação de aplicativos nativos multiplataforma. Além disso, tenho experiência em desenvolver apps para smart tv e smart watches, garantindo interfaces funcionais e atraentes.",
+    icon: AiOutlineMobile,
+  },
+  {
+    id: 3,
+    title: "Backend",
+    desc: "No desenvolvimento backend, domino Node.js, Express e Nest.js, com experiência em bancos de dados como MySQL, MongoDB, Postgres e Oracle SQL. Isso me permite criar servidores robustos e escaláveis para aplicativos web e APIs.",
+    icon: FaNodeJs,
+  },
+  {
+    id: 4,
+    title: "DevOps",
+    desc: "No campo de DevOps, sou experiente em ferramentas como Docker, Kubernetes, Microservices e Skaffold para orquestração e implantação eficiente de aplicativos. Também utilizo Nginx, Git Actions e tenho conhecimento em serviços de nuvem, incluindo AWS, Digital Ocean e Google Cloud, para garantir a entrega contínua e a escalabilidade de sistemas complexos.",
+    icon: BiInfinite,
+  },
+];
