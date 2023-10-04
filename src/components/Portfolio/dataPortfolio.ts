186449a893b04519e3ea1cd13f733dc865e74d85
Adriano Barbosa <adriano.barbosa100@email.com>
2023-10-04 10:38:19 -0300

    feat: change link img

--- src/components/Portfolio/dataPortfolio.ts
+++ src/components/Portfolio/dataPortfolio.ts
@@ -1,7 +1,7 @@
 export const dataPortfolio = [
   {
     id: 1,
-    href: "https://data.terabox.com/thumbnail/d9bce57f2a05580e4a6c8d5b5acbcaf1?fid=4400658442712-250528-743180208552931&time=1695920400&rt=sh&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-e15fkQcXCxkZnOPQvT5OSBFIJdU%3D&expires=8h&chkv=0&chkbd=0&chkpc=&dp-logid=521965783360985546&dp-callid=0&size=c1600_u1600&quality=100&vuk=-&ft=video",
+    href: "https://data.terabox.com/thumbnail/d9bce57f2a05580e4a6c8d5b5acbcaf1?fid=4400658442712-250528-743180208552931&time=1696424400&rt=sh&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-G%2F2ZzYtCwpyOsAHNYHxxqLaMRTk%3D&expires=8h&chkv=0&chkbd=0&chkpc=&dp-logid=8727950869672163428&dp-callid=0&size=c1600_u1600&quality=100&vuk=-&ft=video",
     title: "Sistema Meu Lote Minha Historia",
     desc: "Utilizei Next.js para criar esse sistema avançado. Implementei autenticação por cookies, usei também bibliotecas de mapas e gerenciamento de cadastros de participantes. O sistema controla todas as etapas, incluindo visitas técnicas. No backend, usei Node.js e Express, garantindo segurança com autenticação em rotas para usuários admin e comuns.",
     altImage: "Sistema Meu Lote Historia",
