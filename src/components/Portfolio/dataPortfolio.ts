48d1e936576d45b4e568fb0f3b723b393030eb19
Adriano Barbosa <adriano.barbosa100@gmail.com>
2024-05-15 14:19:22 -0300

    feat: add item in portfolio

--- src/components/Portfolio/dataPortfolio.ts
+++ src/components/Portfolio/dataPortfolio.ts
@@ -2,8 +2,17 @@ export const dataPortfolio = [
   {
     id: 1,
     href: "/mlmh.png",
+    linkUrl: "https://painelmlmh.anapolis.go.gov.br/",
     title: "Sistema Meu Lote Minha Historia",
     desc: "Utilizei Next.js para criar esse sistema avançado. Implementei autenticação por cookies, usei também bibliotecas de mapas e gerenciamento de cadastros de participantes. O sistema controla todas as etapas, incluindo visitas técnicas. No backend, usei Node.js e Express, garantindo segurança com autenticação em rotas para usuários admin e comuns.",
     altImage: "Sistema Meu Lote Historia",
+  },
+  {
+    id: 1,
+    href: "/portalsei.png",
+    linkUrl: "https://portalsei.anapolis.go.gov.br/",
+    title: "Portal SEI Anápolis",
+    desc: "Utilizei React e Vite para criar essa landing page responsiva. Para o CSS, utilizei um framework chamado Material-UI (MUI). Esta landing page conta com um sistema de slides e outras animações.",
+    altImage: "Portal SEI de Anápolis",
   },
 ];
