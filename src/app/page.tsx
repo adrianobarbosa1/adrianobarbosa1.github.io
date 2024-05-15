5a58980bced9d9963a9c489e99425002c5314698
Adriano Barbosa <adriano.barbosa100@gmail.com>
2024-05-15 17:42:31 -0300

    feat: add import aos main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,11 @@ import Footer from "@/components/Footer";
 import Habilidades from "@/components/Habilidades";
 import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
+import "aos/dist/aos.css";
 
 export default function Home() {
   return (
-    <main id="home" className="flex flex-col items-center  mx-auto mt-4">
+    <main id="home" className="mx-auto mt-4 flex  flex-col items-center">
       <Hero />
       <About />
       <Habilidades />
